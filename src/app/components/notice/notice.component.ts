aac97da7d2b28a606c693a03c6f2620dbbdb1658
João Paulo <joaowais@gmail.com>
2024-01-23 18:43:29 -0300

    adiconando data dinamicamente

--- src/app/components/notice/notice.component.ts
+++ src/app/components/notice/notice.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-notice',
   templateUrl: './notice.component.html',
-  styleUrls: ['./notice.component.css']
+  styleUrls: ['./notice.component.css'],
 })
 export class NoticeComponent {
+  dataAtual: Date = new Date();
+  options: Intl.DateTimeFormatOptions = {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  };
 
+  @Input()
+  data: string = this.dataAtual.toLocaleDateString('pt-BR', this.options);
 }
